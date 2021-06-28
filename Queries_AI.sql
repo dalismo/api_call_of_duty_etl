27e70b84da0e3d41a00bc0917d623f9bf5b797ee
adriana-icasiano <81338603+adriana-icasiano@users.noreply.github.com>
2021-06-28 11:26:04 -0400

    updated queries to lower case to match table create

--- Queries_AI.sql
+++ Queries_AI.sql
@@ -1,15 +1,21 @@
-SELECT * FROM Game
+SELECT * FROM esrb_rating
+SELECT * FROM game
+SELECT * FROM sales_platform
+SELECT * FROM ratings
+SELECT * FROM platform
 
 --To retrieve ESRB Ratings for each game with ESRB rating mature
+SELECT e.esrb_rating, g.game_ID, g.game_name
+FROM esrb_rating as e
+INNER JOIN game as g
+ON e.game_ID = g.game_id
+WHERE e.esrb_rating = 'Mature'
 
-SELECT e.ESRB_rating, g.Game_ID, g.Game_Name
-FROM ESRB_Rating as e
-INNER JOIN Game as g
-ON e.Game_ID = g.Game_ID
-WHERE e.ESRB_rating = 'Mature'
-
---To retrieve the most popular games released in the year of 2016
-
-SELECT Game_ID, Game_Name, 
-FROM Game as g
-INNER JOIN 
+--To retrieve the most popular games released between 2012 to 2016
+SELECT g.game_id", g.game_name", sp.platform_id, r.rating, r.metacritic, sp.platform_id
+FROM game as g
+INNER JOIN ratings as r
+ON g.game_id = r.game_id
+INNER JOIN sales_platform as sp
+ON g.game_id = sp.game_id
+ORDER BY r.rating DESC
